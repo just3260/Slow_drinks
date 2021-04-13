c699f61d9aeee78efd7188c4bc7ce9f6c29b8ebf
Andrew Wang <andrew@ins.to>
2021-04-13 17:34:39 +0800

    - Add: create codable

---
+++ Slow Drinks/Client.swift
@@ -0,0 +1,38 @@
+//
+//  Client.swift
+//  Slow Drinks
+//
+//  Created by Andrew Wang on 2021/4/13.
+//
+
+import Foundation
+
+struct Records: Codable {
+    var records: [Fields]
+}
+
+struct Fields: Codable {
+    var id: String
+    var client: Client
+    var createdTime: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case client = "fields"
+        case createdTime = "createdTime"
+    }
+}
+
+struct Client: Codable {
+    var gender: String
+    var amount: Int
+    var list: Int
+    var item: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case gender = "性別"
+        case amount = "金額"
+        case list = "List"
+        case item = "品項"
+    }
+}
--- Slow Drinks/ViewController.swift
+++ Slow Drinks/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import Alamofire
+
+
+struct Login: Encodable {
+    let email: String
+    let password: String
+}
 
 class ViewController: UIViewController {
-
+    
+    
+    let url = "https://api.airtable.com/v0/appj0XaBDz5v4Y8OG/Table%201"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer keyJ7HuUvcwB2hicx",
+            "Accept": "application/json"
+        ]
+        
+        AF.request(url, method: .get, headers: headers).responseJSON { (response) in
+            print(response)
+            
+            
+//            let decoder = JSONDecoder()
+//            if let loadedPerson = try? decoder.decode(Records.self, from: response) {
+//
+//            }
+            
+            
+//            if response.result.isSuccess {
+                
+                let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+                
+                if let result = response.data, let results = try? decoder.decode(Records.self, from: result) {
+                    for client in results.records {
+                        print(client)
+                    }
+                }
+//            }
+            
+            
+//            do {
+//                let data = try JSONSerialization.data(withJSONObject:response, options: .prettyPrinted)
+//                let str = String(data:data, encoding: .utf8)
+//                print(str)
+//            } catch {
+//                print("JSON error")
+//            }
+        }
+        
+        
     }
-
-
+    
+    
 }
 
